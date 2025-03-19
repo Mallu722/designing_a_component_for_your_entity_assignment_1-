c4749f617a807009186f36b2733688d8e2e95c05
Mallikarjun722 <anandhiremath979@gmail.com>
2025-03-20 00:27:48 +0530

    userCard

--- src/components/usercard.jsx
+++ src/components/usercard.jsx
@@ -1,1 +1,18 @@
 // write the component code here
+// write the component code here
+function Usercard() {
+    return (
+      <div>
+        <div>
+          <img src="https://i.pinimg.com/736x/f3/85/d7/f385d78eba93e8b768bcc04bf96fe5a5.jpg" height="220px" width="220px"/>
+        </div>
+  
+        <h1>Prish</h1>
+        <h3>prishit@gmail.com</h3>
+        <h3>9883299212</h3>
+        <p><strong>Plot no 76/23-53, Mumbai</strong></p>
+      </div>
+    );
+  }
+  
+  export default Usercard
